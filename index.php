3b678b6c0e96b025e404a49df4520f1adda746c6
RizkiTrisna <rzkid.ra@gmail.com>
2020-05-09 11:46:53 +0700

    update fitur get data by key and id_kota

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oa
     return $connection;
 }
 
-function getData($words)
+function getData($words, $id_kota)
 {
     global $consumerkey;
     global $consumersecret;
@@ -26,111 +26,82 @@ function getData($words)
     global $accesstokensecret;
     global $pdo_con;
 
-    $pdo_statement_kota = $pdo_con->prepare("SELECT id_kota FROM kota");
-    $pdo_statement_kota->execute();
-    $result_kota = $pdo_statement_kota->fetchAll();
-    foreach ($result_kota as $row_kota) {
+    echo "getting access...";
+    echo "<br>";
+    $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+    $next_url = "";
+    $success_count = 0;
+    $failed_count = 0;
+    $year = "";
+    $month = "";
+    $date = "";
 
-        $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
-        $next_url = "";
-        $success_count = 0;
-        $failed_count = 0;
-        $year = "";
-        $month = "";
-        $date = "";
+    //Mencari lokasi lat dan lon berdasar id kota
+    $id_kota = $id_kota;
+    $pdo_statement_loc = $pdo_con->prepare("SELECT * FROM kota WHERE id_kota=:id_kota");
+    $pdo_statement_loc->bindParam(":id_kota", $id_kota);
+    $pdo_statement_loc->execute();
+    $result_loc = $pdo_statement_loc->fetchAll();
+    $lat = $result_loc[0]['lat'];
+    $lon = $result_loc[0]['lon'];
+    $range = $result_loc[0]['radius_cari'];
+    //Inisialisasi awal tanggal
+    $end_date = date("Y-M-d", strtotime("today -7 days"));
+    $current_date = date("Y-M-d", strtotime("today"));
+    $message = "";
+    //Pencarian tweet
+    $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=$words&geocode=$lat,$lon," . $range . "km&count=95");
+    $hasil_encode = json_encode($tweets);
+    $hasil_decode = json_decode(json_encode($tweets), true);
+    echo "access established!";
+    echo "<br>";
+    if (isset($hasil_decode["statuses"]) && isset($hasil_decode["search_metadata"]["next_results"])) {
+        echo "start crawling...";
+        echo "<br>";
+        if (inputCrawlToDatabase($hasil_encode, $id_kota)) {
+            $next_url = $hasil_decode["search_metadata"]["next_results"];
+            $success_count += 1;
 
-        //Mencari lokasi lat dan lon berdasar id kota
-        $id_kota = $row_kota['id_kota'];
-        $pdo_statement_loc = $pdo_con->prepare("SELECT * FROM kota WHERE id_kota=:id_kota");
-        $pdo_statement_loc->bindParam(":id_kota", $id_kota);
-        $pdo_statement_loc->execute();
-        $result_loc = $pdo_statement_loc->fetchAll();
-        $lat = $result_loc[0]['lat'];
-        $lon = $result_loc[0]['lon'];
-        $range = $result_loc[0]['radius_cari'];
-        //Inisialisasi awal tanggal
-        $end_date = date("Y-M-d", strtotime("today -7 days"));
-        $current_date = date("Y-M-d", strtotime("today"));
+            $year = substr($hasil_decode["statuses"][0]['created_at'], -5);
+            $month = substr($hasil_decode["statuses"][0]['created_at'], 4, 3);
+            $date = substr($hasil_decode["statuses"][0]['created_at'], 8, 2);
 
-        //Pencarian tweet
-        $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=$words&geocode=$lat,$lon," . $range . "km&count=95");
-        $hasil_encode = json_encode($tweets);
-        $hasil_decode = json_decode(json_encode($tweets), true);
+            $current_date = date("Y-M-d", strtotime($month . " " . $date . " " . $year));
+            $status = true;
+            $isset_statuses = 1;
+            while (($end_date != $current_date) && $status && $isset_statuses) {
 
-        if (isset($hasil_decode["statuses"]) && isset($hasil_decode["search_metadata"]["next_results"]) ) {
-            if (inputCrawlToDatabase($hasil_encode, $id_kota)) {
-                $next_url = $hasil_decode["search_metadata"]["next_results"];
-                $success_count += 1;
+                $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json" . $next_url);
+                $hasil_encode = json_encode($tweets);
+                $hasil_decode = json_decode(json_encode($tweets), true);
 
-                $year = substr($hasil_decode["statuses"][0]['created_at'], -5);
-                $month = substr($hasil_decode["statuses"][0]['created_at'], 4, 3);
-                $date = substr($hasil_decode["statuses"][0]['created_at'], 8, 2);
+                // Salah di next url nya, coba cek++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                if (($isset_statuses = isset($hasil_decode["statuses"])) && isset($hasil_decode["search_metadata"]["next_results"]) && ($status = inputCrawlToDatabase($hasil_encode, $id_kota))) {
 
-                $current_date = date("Y-M-d", strtotime($month . " " . $date . " " . $year));
-                $status = true;
+                    try {
+                        $next_url = $hasil_decode["search_metadata"]["next_results"];
+                        $success_count += 1;
+                        $year = substr($hasil_decode["statuses"][0]['created_at'], -5);
+                        $month = substr($hasil_decode["statuses"][0]['created_at'], 4, 3);
+                        $date = substr($hasil_decode["statuses"][0]['created_at'], 8, 2);
 
-                while (($end_date != $current_date) && $status) {
-
-                    $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json" . $next_url);
-                    $hasil_encode = json_encode($tweets);
-                    $hasil_decode = json_decode(json_encode($tweets), true);
-
-                    // Salah di next url nya, coba cek++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-                    if (isset($hasil_decode["statuses"]) && isset($hasil_decode["search_metadata"]["next_results"]) && ($status = inputCrawlToDatabase($hasil_encode, $id_kota))) {
-                        
-                        try {
-                            $next_url = $hasil_decode["search_metadata"]["next_results"];
-                            $success_count += 1;
-                            $year = substr($hasil_decode["statuses"][0]['created_at'], -5);
-                            $month = substr($hasil_decode["statuses"][0]['created_at'], 4, 3);
-                            $date = substr($hasil_decode["statuses"][0]['created_at'], 8, 2);
-
-                            $current_date = date("Y-M-d", strtotime($month . " " . $date . " " . $year));
-                        } catch (Exception $e) {
-                            $e->getMessage();
-                            die();
-                        }
-                    } else {
-                        echo "hasil_decode['statuses'] di if dalam while belum di set";
+                        $current_date = date("Y-M-d", strtotime($month . " " . $date . " " . $year));
+                    } catch (Exception $e) {
+                        $e->getMessage();
                     }
+                } else {
+                    $message = "hasil_decode['statuses'] di if dalam while belum di set";
                 }
             }
-        } else {
-            echo "hasil_decode['statuses'] di if luar while belum di set";
         }
-
-        // Reserved
-        // for ($i = 1; $i <= $times; $i++) {
-        //     if ($i == 1) {
-
-        //         $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?q=Pendidikan&geocode=$lat,$lon," . $range . "km&count=95");
-        //         $hasil_encode = json_encode($tweets);
-        //         $hasil_decode = json_decode(json_encode($tweets), true);
-
-        //         echo $hasil_encode;
-
-        //         if ($hasil_encode != null) {
-        //             inputCrawlToDatabase($hasil_encode, $id_kota);
-        //             $next_url = $hasil_decode["search_metadata"]["next_results"];
-        //             $success_count += 1;
-        //         }
-        //     } else if ($i != 1) {
-        //         $tweets = $connection->get("https://api.twitter.com/1.1/search/tweets.json?" . $next_url);
-        //         $hasil_encode = json_encode($tweets);
-        //         $hasil_decode = json_decode(json_encode($tweets), true);
-
-        //         echo $hasil_encode;
-
-        //         if ($hasil_encode != null) {
-        //             inputCrawlToDatabase($hasil_encode, $id_kota);
-        //             $next_url = $hasil_decode["search_metadata"]["next_results"];
-        //             $success_count += 1;
-        //         }
-        //     }
-        // }
-            echo "Last URL : ".  $next_url ." On id_kota : ".$id_kota;
+    } else if (isset($hasil_decode['errors'][0]['message'])) {
+        $message = $hasil_decode['errors'][0]['message'];
+    } else {
+        $message = "hasil_decode['statuses'] di if luar while belum di set";
     }
-    return $failed_count;
+    $message .= " - Last URL : " .  $next_url . " On id_kota : " . $id_kota;
+
+    return $message;
 }
 
 function start()
@@ -138,8 +109,8 @@ function start()
     global $pdo_con;
 
     $words = 'Pendidikan';
-    echo "Failed count" . getData($words);
-    echo "\n";
+    echo "Note : " . getData($words, 11);
+    echo "<br>";
 }
 
 start();
